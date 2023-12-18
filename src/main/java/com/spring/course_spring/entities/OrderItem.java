e608339e93a1b4937835d197b0697f529bd2c7dd
welintonborges <welinton.b.borges@gmail>
2023-12-18 16:06:30 -0300

    Payment, one-to-one association

--- src/main/java/com/spring/course_spring/entities/OrderItem.java
+++ src/main/java/com/spring/course_spring/entities/OrderItem.java
@@ -38,6 +38,7 @@ public class OrderItem implements Serializable {
     }
 
 
+
     public Product getProduct(){
         return id.getProduct();
     }
--- src/main/java/com/spring/course_spring/repositories/OrderItemRepository.java
+++ src/main/java/com/spring/course_spring/repositories/OrderItemRepository.java
@@ -1,8 +1,8 @@
 package com.spring.course_spring.repositories;
 
-import com.spring.course_spring.entities.Category;
 import com.spring.course_spring.entities.OrderItem;
+import com.spring.course_spring.entities.pk.OrderItemPk;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPk> {
 }
