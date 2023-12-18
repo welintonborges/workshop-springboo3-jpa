f752c541240146b5266b2ed2da1a5abf0696d340
welintonborges <welinton.b.borges@gmail>
2023-12-18 16:39:21 -0300

    Payment

--- src/main/java/com/spring/course_spring/entities/Order.java
+++ src/main/java/com/spring/course_spring/entities/Order.java
@@ -30,7 +30,8 @@ public class Order implements Serializable {
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
-
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
     public Order() {
     }
 
@@ -72,6 +73,14 @@ public class Order implements Serializable {
     public void setOrderStatus(OrderStatus orderStatus) {
         if (orderStatus != null)
           this.orderStatus = orderStatus.getCode();
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems() {
