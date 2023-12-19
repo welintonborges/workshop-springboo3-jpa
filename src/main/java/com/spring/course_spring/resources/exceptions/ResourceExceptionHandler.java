a034d513b08dd62f1215128883e7434cd4c066b2
p_920763 <welinton.borges@camara.leg.br>
2023-12-19 16:00:37 -0300

    Exception handling - findById

---
+++ src/main/java/com/spring/course_spring/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.spring.course_spring.resources.exceptions;
+
+import com.spring.course_spring.services.exceptions.ResourceNotFoundException;
+import com.sun.net.httpserver.HttpsServer;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandarError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        String error = "Resouce  not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandarError err = new StandarError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/com/spring/course_spring/services/UserService.java
+++ src/main/java/com/spring/course_spring/services/UserService.java
@@ -2,6 +2,7 @@ package com.spring.course_spring.services;
 
 import com.spring.course_spring.entities.User;
 import com.spring.course_spring.repositories.UserRepository;
+import com.spring.course_spring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findBayId(Long id){
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
