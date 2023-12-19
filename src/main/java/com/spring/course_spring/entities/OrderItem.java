2e687fd95b229583d73aa048726b91e1aab927fc
p_920763 <welinton.borges@camara.leg.br>
2023-12-19 12:58:00 -0300

    Subtotal & total methods

--- src/main/java/com/spring/course_spring/entities/OrderItem.java
+++ src/main/java/com/spring/course_spring/entities/OrderItem.java
@@ -70,6 +70,9 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    public  Double getSubTotal(){
+        return price * quantity;
+    }
 
     @Override
     public boolean equals(Object o) {
