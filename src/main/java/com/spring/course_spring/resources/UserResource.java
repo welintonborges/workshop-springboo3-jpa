0be7c0beddd5c89c03d08a46542c22b1781e6ad6
p_920763 <welinton.borges@camara.leg.br>
2023-12-19 13:41:00 -0300

    User Update.

--- src/main/java/com/spring/course_spring/resources/UserResource.java
+++ src/main/java/com/spring/course_spring/resources/UserResource.java
@@ -22,23 +22,25 @@ public class UserResource {
         List<User> list = userService.findAll();
         return ResponseEntity.ok().body(list);
     }
-
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = userService.findBayId(id);
         return ResponseEntity.ok().body(obj);
     }
-
     @PostMapping
     public ResponseEntity<User> insert(@RequestBody User obj){
         obj = userService.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
-
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable  Long id){
             userService.delete(id);
             return ResponseEntity.noContent().build();
+    }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+        obj = userService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 }
